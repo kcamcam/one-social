41d9126503a9f9230fedc264c35867b7b71ce3f0
Kevin Camellini <kevincamellini@gmail.com>
2018-03-16 03:51:14 -0400

    assets procmpile for production

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require turbolinks
 //= require jquery3
 //= require sweetalert2
 //= require sweet-alert2-rails
+//= require rails-ujs
+//= require turbolinks
 //= require_tree .
