228cdeee4ab446f900d2b8bdac4c9e441712c803
Kevin Camellini <kevincamellini@gmail.com>
2018-03-19 03:51:38 -0400

    add new scoail media page styling

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,8 +1,10 @@
 function socialfn(platform,anchor){
-  var socialradio = document.getElementById('social-radio');
-  socialradio.name = 'social_medium['+platform+']';
-  if (anchor != "")
-    socialradio.value = anchor+"";
-  else
-    socialradio.value = "New"+platform;
+	var socialradio = document.getElementById('social-radio');
+	socialradio.name = 'social_medium['+platform+']';
+	if (anchor != "")
+		socialradio.value = anchor+"";
+	else{
+		socialradio.value = "";
+		socialradio.placeholder = "Enter a new "+platform+" username";
+	}
 }
