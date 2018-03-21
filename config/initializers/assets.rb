002b3a2819d6f6e70662481c75d6b4725d713d70
Kevin Camellini <kevincamellini@gmail.com>
2018-03-20 23:41:01 -0400

    moved all assets to the rails pipeline

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,3 +13,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( fontawesome-all.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( popper.min.js )
+Rails.application.config.assets.precompile += %w( typed.js )
+Rails.application.config.assets.precompile += %w( custom.css )
