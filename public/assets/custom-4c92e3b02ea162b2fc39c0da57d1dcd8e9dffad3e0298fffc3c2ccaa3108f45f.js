eecd34e6774fbab5acbe5929ce87bab9855e3e64
Kevin Camellini <kevincamellini@gmail.com>
2018-03-20 23:59:46 -0400

    fixing assets not loading

---
+++ public/assets/custom-4c92e3b02ea162b2fc39c0da57d1dcd8e9dffad3e0298fffc3c2ccaa3108f45f.js
@@ -0,0 +1,1 @@
+function socialfn(e,t){var u=document.getElementById("social-radio");u.name="social_medium["+e+"]",""!=t?u.value=t+"":(u.value="",u.placeholder="Enter a new "+e+" username"),"youtube"==e||"googleplus"==e||"linkedin"==e?$(".input-group-text").text("/"):"bitcoin"==e||"ethereum"==e?$(".input-group-text").text("#"):$(".input-group-text").text("@")}
