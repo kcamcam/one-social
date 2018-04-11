e392ba4353b0776968ede9f274b61b9358104be2
Kevin Camellini <kevincamellini@gmail.com>
2018-04-11 01:13:30 -0400

    changed the order of appearance of socail media on different pages

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,6 +9,6 @@ class WelcomeController < ApplicationController
   end
 
   def index
-    @socialarray = ["instagram","twitter","youtube","facebook","googleplus","linkedin","snapchat","messenger","wire","spotify","soundcloud","pinterest","flickr","tumblr","medium","reddit","hackernews","github","keybase","devto","devpost","angel","twitch","origin","steam","discord","website","paypal","bitcoin","ethereum"]
+    @socialarray = ["instagram","twitter","youtube","facebook","googleplus","linkedin","snapchat","messenger","wire","spotify","soundcloud","pinterest","flickr","tumblr","reddit","medium","hackernews","github","keybase","devto","devpost","angel","twitch","origin","steam","discord","website","paypal","bitcoin","ethereum"]
   end
 end
